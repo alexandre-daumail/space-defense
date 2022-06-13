bfc9c57280f7f672ef02c8ddea33f1ff0c1daaf9
alexandre-daumail <alexandre.daumail@laplateforme.io>
2022-06-13 18:22:06 +0200

    description added
    Vessel class, pairing() method

--- ANSWERS.md
+++ ANSWERS.md
@@ -1,1 +1,16 @@
-# space-defense
+# Answers
+
+Here are my answers to the questions asked at the end of the test document :
+
+## Q&A
+
+#### 1. What is the complexity of your algorithm (in Big O notation)?
+
+my algorithm (pairing method) has two loops and 3 
+
+#### 2. How would you improve your algorithm?
+
+
+
+#### 3. How would your adapt your algorithm to three dimensions? How would that affect the complexity? 
+
--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <?php
 
 /**
-  * Vessel
-  * 
-  * Description for the class
-  * @property string $id used to identify the vessel
-  * @property int $pairedVesselId used 
-  * @property array $position Description for bar
-*/
+ * Vessel
+ *
+ * This class is common to both Offensivecrafts and Supportcrafts classes because they share some properties.
+ * 
+ * @property string $id used to identify the differtent vessels
+ * @property int $pairedVesselId stores the Vessel ID that each vessel has when we use the pairing method from Fleet class.
+ * @property array $position stores the position from the vessel in the (x,y) layout.
+ */
 class Vessel
 {
   public $id;
@@ -25,14 +26,14 @@ class OffensiveCraft extends Vessel
 {
   public $numberOfCanons;
   public $shieldPower = 100;
-  public $offensiveType = ['battleships', 'cruisers', 'destroyers'];
+  public $offensiveType ;
 }
 
 class SupportCraft extends Vessel
 {
   public $name;
   public $medicUnit;
-  public $supportType = ['refueling', 'mechanical assistance', 'cargo'];
+  public $supportType ;
 }
 
 /**
@@ -41,19 +42,21 @@ class SupportCraft extends Vessel
 class Fleet
 {
   public array $fleetLocationData = [];
-  
+
   public int $offCrafts;
   public int $suppCrafts;
   public int $totalCrafts;
-    
+
   /**
    * __construct
+   * 
+   * 
    *
    * @param  int $layoutSize
    * @param  int $offCrafts
    * @param  int $suppCrafts
    * @return void
-  */
+   */
   public function __construct(int $layoutSize, int $offCrafts,  int $suppCrafts)
   {
 
@@ -78,49 +81,48 @@ class Fleet
     $offCraftCoordinates = [];
     $suppCraftCoordinates = [];
 
-    for ($i = 0; $i < $this->totalCrafts/2; $i++) {
+    for ($i = 0; $i < $this->totalCrafts / 2; $i++) {
 
-        $id = "OFF n°" . $i;
-        $this->offCraftFleet[$i] = new OffensiveCraft($id, [$random_x[$i], $random_y[$i]]);
-        array_push($offCraftCoordinates, ["id" => $id, "x" => $random_x[$i], "y" => $random_y[$i]]);
-        
-        $id = "SUPP n°" . $i;
-        $this->suppCraftFleet[$i] = new SupportCraft($id, [$random_x[$i], $random_y[$i]]);
-        array_push($suppCraftCoordinates, ["id" => $id, "x" => $random_x[$i], "y" => $random_y[$i]]);
-      } 
-    
-    
+      $id = "OFF n°" . $i;
+      $this->offCraftFleet[$i] = new OffensiveCraft($id, [$random_x[$i], $random_y[$i]]);
+      array_push($offCraftCoordinates, ["id" => $id, "x" => $random_x[$i], "y" => $random_y[$i]]);
+
+      $id = "SUPP n°" . $i;
+      $this->suppCraftFleet[$i] = new SupportCraft($id, [$random_x[$i], $random_y[$i]]);
+      array_push($suppCraftCoordinates, ["id" => $id, "x" => $random_x[$i], "y" => $random_y[$i]]);
+    }
+
+
     $this->fleetLocationData = array_merge($offCraftCoordinates, $suppCraftCoordinates);
-    
+
     return $this->fleetLocationData;
-    
   }
 
   /**
- * distance
- *
- * @param  array $v1
- * @param  array $v2
- * @return float
- */
-public function distance(array $v1, array $v2) : float
-{
+   * distance
+   *
+   * @param  array $v1
+   * @param  array $v2
+   * @return float
+   */
+  public function distance(array $v1, array $v2): float
+  {
 
-  return sqrt(
+    return sqrt(
 
-    (($v1[0] - $v2[0]) * ($v1[0] - $v2[0])) + (($v1[1] - $v2[1]) * ($v1[1] - $v2[1]))
+      (($v1[0] - $v2[0]) * ($v1[0] - $v2[0])) + (($v1[1] - $v2[1]) * ($v1[1] - $v2[1]))
 
-  );
-
+    );
   }
 
-  //ensuite, on cherche le supp le plus proche
-//on fait défiler tous les offCraft
-//une fois trouvé, on le positionne à côté (si la position est à côté, distance() retourne 1 ou  racine de sqrt(2), les navires sont adjacents)
-//on change la position du suppCraft en enlevant sa position de l'array $suppCraft et on le stocke 
-// 
-
-  public function pairing ()
+  /**
+   * Pairing
+   *
+   * In a first loop, the algorithm goes through all the Offensivecrafts. The second loop processes first the distance between the current crafts if they are not paired yet. If distance() returns 1 or the square root of 2, the crafts are already positioned next to each other. If not, the algorithm continues to run and compares current offcraft to the other suppCrafts and stores the distance between them in $minDistance each time it's smaller then the existing $minDistance. During this process, the algo also store the index $j each time a new minDistance is stored.
+   * 
+   * @return void
+   */
+  public function pairing()
   {
     $minDistance = null;
     $pair = null;
@@ -129,41 +131,37 @@ public function distance(array $v1, array $v2) : float
 
       for ($j = 0; $j < $this->suppCrafts; $j++) {
 
-        $distance = $this->distance($this->offCraftFleet[$i]->position, $this->suppCraftFleet[$j]->position);
+        if ($this->suppCraftFleet[$j]->pairedVesselId == null) {
 
-        if ($distance == 1 || $distance == sqrt(2)) {
+          $distance = $this->distance($this->offCraftFleet[$i]->position, $this->suppCraftFleet[$j]->position);
 
-          $pair = $j;
-          // $this->offCraft[$i]->pairedVesselId = $this->suppCraft[$j]->id;
-          // $this->suppCraft[$j]->pairedVesselId = $this->offCraft[$i]->id;
+          if ($distance == 1 || $distance == sqrt(2)) {
 
-        } else if ($j == 0){
+            $pair = $j;
+          } else if ($j == 0) {
 
-          $pair = $j;
-          $minDistance = $distance;
-          
-        } else if($distance < $minDistance) {
+            $pair = $j;
+            $minDistance = $distance;
+          } else if ($distance < $minDistance) {
 
-          $pair = $j;
-          $minDistance = $distance;
+            $pair = $j;
+            $minDistance = $distance;
+          }
+        
 
-        } 
-
+        $this->offCraftFleet[$i]->pairedVesselId = $this->suppCraftFleet[$pair]->id;
+        $this->suppCraftFleet[$pair]->pairedVesselId = $this->offCraftFleet[$i]->id;
+        }
       }
-
-      $this->offCraftFleet[$i]->pairedVesselId = $this->suppCraftFleet[$pair]->id;
-      $this->suppCraftFleet[$pair]->pairedVesselId = $this->offCraftFleet[$i]->id;  
-    
     }
   }
 }
 
 
-$fleet = new Fleet (100, 5, 5);
+$fleet = new Fleet(100, 5, 5);
 
 $fleet->pairing();
 
- echo "<pre>";
- var_dump($fleet->suppCraftFleet);
- echo "</pre>";
-
+echo "<pre>";
+var_dump($fleet->suppCraftFleet);
+echo "</pre>";
